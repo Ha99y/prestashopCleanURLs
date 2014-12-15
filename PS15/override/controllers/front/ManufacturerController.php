58aed04f71dc25656b795a81757c02fcb045dc9a
Michael Hjulskov <michael@hjulskov.dk>
2014-12-15 15:50:54 +0100

    make it so that it can handle a manufacturer name like "B&O"

--- PS15/override/controllers/front/ManufacturerController.php
+++ PS15/override/controllers/front/ManufacturerController.php
@@ -31,17 +31,33 @@ class ManufacturerController extends ManufacturerControllerCore
 			// DB for link_rewrite for manufacturers
 			// Should we use the Mysql FullText Index Search ??
 			//
-			$sql = 'SELECT m.`id_manufacturer`
+			$sql = 'SELECT m.`id_manufacturer`, REPLACE(m.`name`,'&','') as manufacturer_name
 				FROM `'._DB_PREFIX_.'manufacturer` m
 				LEFT JOIN `'._DB_PREFIX_.'manufacturer_shop` s ON (m.`id_manufacturer` = s.`id_manufacturer`)
-				WHERE m.`name` LIKE \''.$name_manufacturer.'\'';
+				WHERE manufacturer_name LIKE \''.$name_manufacturer.'\'';
 
 			if (Shop::isFeatureActive() && Shop::getContext() == Shop::CONTEXT_SHOP)
 			{
 				$sql .= ' AND s.`id_shop` = '.(int)Shop::getContextShopID();
 			}
 
-			$id_manufacturer = Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
+			$manufacturers_list = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
+			$manufacturers_count = count($manufacturers_list);
+
+			if($manufacturers_count == 1){
+				// Found ONLY one so dont need to check anything
+				$id_manufacturer = (int)$manufacturers_list[0]['id_manufacturer'];
+				$_GET['noredirect'] = 1;
+			} else if($manufacturers_count > 1){
+				// Found more than one so ...
+				// yeah I actually dont know what to do :(
+				// so lets just grab the first one matching
+				$id_manufacturer = (int)$manufacturers_list[0]['id_manufacturer'];
+			} else {
+				// none found
+				$id_manufacturer = 0;
+			}
+
 
 			if($id_manufacturer > 0)
 			{
